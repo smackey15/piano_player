4815676602f5aee04586ee5d724f45789b05c869
sean <midruss@yahoo.com>
2022-02-08 18:05:05 -0500

    Added line 18 to index.js so that the only plays and continues to play once when the user holds the key down longer.  Refactored keyup event listener into a two liner

--- src/index.js
+++ src/index.js
@@ -15,15 +15,46 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
   document.addEventListener("keydown", e => {
-    if (e.key === "a") {
-      synth.triggerAttack("A3")
-    }
+    if (e.repeat) return;
+
+    if (e.key === "a") synth.triggerAttack("A3")
+  
+    if (e.key === "w") synth.triggerAttack("A#3")
+  
+    if (e.key === "s") synth.triggerAttack("B3")
+    
+    if (e.key === "d") synth.triggerAttack("C4")
+    
+    if (e.key === "r") synth.triggerAttack("C#4")
+    
+    if (e.key === "f") synth.triggerAttack("D4")
+    
+    if (e.key === "t") synth.triggerAttack("D#4")
+    
+    if (e.key === "g") synth.triggerAttack("E4")
+    
+    if (e.key === "h") synth.triggerAttack("F4")
+    
+    if (e.key === "u") synth.triggerAttack("F#4")
+    
+    if (e.key === "j") synth.triggerAttack("G4")
+    
+    if (e.key === "i") synth.triggerAttack("G#4")
+    
+    if (e.key === "k") synth.triggerAttack("A4")
+    
+    if (e.key === "o") synth.triggerAttack("A#4")
+    
+    if (e.key === "l") synth.triggerAttack("B4")
+    
+    if (e.key === ";") synth.triggerAttack("C5")
+
+    // e.key.classList.add("pressed"); doesn't work yet.
   });
 
   document.addEventListener("keyup", e => {
-    if (e.key === "a") {
-      synth.triggerRelease();
-    }
+    let keys = ["a", "w", "s", "d", "r", "f", "t", "g", "h", "u", "j", "i", "k", "o", "l", ";"]
+    if (keys.includes(e.key)) synth.triggerRelease();
   });
 
 }); 
